521ca2000383d10da9c2d9ab48c20fecabb0fa18
Cherrypick14 <cherrypicklet@gmail.com>
2024-01-26 00:38:59 +0300

    import likebutton component

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import LikeButton from "./like-button";
       
  function Header({ title }) {
     return <h1>{title ? title : "Default title"}</h1>
@@ -7,11 +7,6 @@ import {useState} from 'react'
  function HomePage() {
     const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"]
    
-    const [likes, setLikes] = useState(0)
-   
-     function handleClick() {
-     setLikes(likes + 1)
-  }
           return (
             <div>
               <Header title="Develop. Preview. Ship." />
@@ -20,8 +15,7 @@ import {useState} from 'react'
                   <li key={name}>{name}</li>
                 ))}
               </ul>
-   
-              <button onClick={handleClick}>Like ({likes})</button>
+              <LikeButton />
             </div>
           );
         }
